abf9be58ef1de9658bcd0379174f61107948ab45
404kevnotfound <student@HSiMac4-7.local>
2018-10-11 09:36:06 -0600

    added chapter 1

---
+++ Chapter 1/Ex_1_5.java
@@ -0,0 +1,12 @@
+/*
+Author: Miriah Thompson
+Date: 9/19/18
+
+Compute expression
+*/
+
+class Ex_1_5 {
+	public static void main(String[] args) {
+		System.out.println( ( (9.5 * 4.5) - (2.5 * 3) ) / (45.5 - 3.5) );
+	}
+}
