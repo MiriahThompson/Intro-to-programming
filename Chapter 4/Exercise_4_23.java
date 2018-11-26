0ecd9bf0be9a9b7f1b2689879a72b8b1b38d18e7
404kevnotfound <student@HSiMac4-7.local>
2018-11-26 09:47:17 -0700

    added file
    added file

---
+++ Chapter 4/Exercise_4_23.java
@@ -0,0 +1,11 @@
+/* 
+Author: Miriah Thompson
+Date:11/19/18
+*/
+import java.util.Scanner;
+class Exercise_4_23 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+	}
+}
